1bb0200cac5ad7fdc8e122740d02c445d6b235e1
dollys21 <contact.dollys@gmail.com>
2017-05-12 16:14:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = 8080;
+var server_ip_address ='127.0.0.1';
 
 app.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
